b8c3dd48d32f875f9f990379e84991a862f9585d
BlueClau <mihai.claudiu66@yahoo.com>
2025-02-06 20:37:00 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import Navbar from "./components/navbar/navbar"
+import "./layout.scss"
+import HomePage from "./routes/homePage/homePage"
+
 function App() {
   return (
-    <div>Hello World</div>
+    <div className="layout">
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      
+      <div className="content">
+        <HomePage/>
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App 
